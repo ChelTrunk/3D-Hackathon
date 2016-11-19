834778dc42402cde3bbe0ce139c58d359673987c
Aidan Globus <aidanglobus@gmail.com>
2016-11-19 13:56:24 -0500

    Updating

--- GroupChess/Test.js
+++ GroupChess/Test.js
@@ -1,5 +1,5 @@
 //move is string in form "original-new"
-//for example move = "0,7-0,5" would call for the A2 bishop to move up to to A4
+//for example move = "A2-A4" would call for the A2 bishop to move up to to A4
 
 //prereq is valid move
 function updateBoard(move){
