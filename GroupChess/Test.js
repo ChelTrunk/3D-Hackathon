a6e0940505109c4975b07b5114a5c035ed2f7d11
Aidan Globus <aidanglobus@gmail.com>
2016-11-19 13:54:32 -0500

    Testing

---
+++ GroupChess/Test.js
@@ -0,0 +1,15 @@
+//move is string in form "original-new"
+//for example move = "0,7-0,5" would call for the A2 bishop to move up to to A4
+
+//prereq is valid move
+function updateBoard(move){
+  //void return
+}
+
+function isValidMove(move){
+
+}
+
+function causesCheck(move){
+
+}
